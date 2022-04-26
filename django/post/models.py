1e21b3197d43bcb1f0b88f8a08338671f31adf46
Kc pele <fidekg123@gmail.com>
2022-04-26 22:16:25 +0000

    post view created

--- django/post/models.py
+++ django/post/models.py
@@ -22,8 +22,8 @@ class Tag(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse("tags", args=[self.slug])
+    # def get_absolute_url(self):
+        # return reverse("tags", args=[self.slug])
 
     def save(self, *args, **kwargs):
         if not self.slug:
--- django/post/views.py
+++ django/post/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from post.models import Post, Stream
+from django.template import loader
+from django.http import HttpResponse
 # Create your views here.
+
+@login_required
+def index(request):
+    user = request.user
+    posts = Stream.objects.filter(user=user)
+    
+    group_ids = []
+    for post in posts:
+        group_ids.append(post.post.id)
+    
+    post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')
+    tempale = loader.get_template('index.html')
+
+    context = {
+        'post_items': post_items
+    }
+    return HttpResponse(tempale.render(context, request))
+
